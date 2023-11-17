bb10ce67d38c07866b24974e83834ed75b45c46f
JP <jp@example.com>
2023-11-16 22:27:07 -0500

    .

---
+++ js/frontpage.js
@@ -0,0 +1,12 @@
+// Navigation Toggle Function
+function toggleNav() {
+  var navList = document.getElementById("nav-list");
+
+  if (navList.classList.contains("hide")) {
+    navList.classList.remove("hide");
+    navList.classList.add("show");
+  } else {
+    navList.classList.remove("show");
+    navList.classList.add("hide");
+  }
+}
