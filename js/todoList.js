458b297c04217a0f73410c31c7bdc394199cc330
A-NIZZY <aaroncrearer4202@gmail.com>
2023-12-08 00:14:16 -0500

    little changes

--- js/todoList.js
+++ js/todoList.js
@@ -1,20 +1,57 @@
+// const taskInput = document.getElementById("taskInput");
+// const taskList = document.getElementById("taskList");
+
+// function addTask() {
+//   const taskText = taskInput.value.trim();
+//   if (taskText !== "") {
+//     const li = document.createElement("li");
+//     li.textContent = taskText;
+//     taskList.appendChild(li);
+//     taskInput.value = "";
+//     li.addEventListener("click", completeTask);
+
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.textContent = "Delete";
+//     deleteBtn.addEventListener("click", deleteTask);
+//     li.appendChild(deleteBtn);
+//   }
+// }
+
+// function deleteTask(event) {
+//   const task = event.target.parentElement;
+//   taskList.removeChild(task);
+// }
+
+// function completeTask(event) {
+//   const task = event.target;
+//   task.classList.toggle("completed");
+// }
+
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
 function addTask() {
   const taskText = taskInput.value.trim();
   if (taskText !== "") {
-    const li = documnet.createElement("li");
+    const li = document.createElement("li");
     li.textContent = taskText;
-    taskList.appendChild(li);
-    taskInput.value = "";
-  }
-  const deleteBtn = document.createElement("button");
-  deletebtn.textContent = "Delete";
-  deletebtn.addEventListener("click, deleteTask");
-  li.appendChild(deletebtn);
 
-  li.addEventListener("click", completeTask);
+    const completeBtn = document.createElement("button");
+    completeBtn.textContent = "✓";
+    completeBtn.addEventListener("click", completeTask);
+    li.appendChild(completeBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑️";
+    deleteBtn.addEventListener("click", deleteTask);
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    li.addEventListener("click", function () {
+      li.classList.toggle("completed");
+    });
+  }
 }
 
 function deleteTask(event) {
@@ -23,6 +60,6 @@ function deleteTask(event) {
 }
 
 function completeTask(event) {
-  const task = event.target;
-  task.classList.toggle("completed");
+  const listItem = event.target.parentElement;
+  listItem.classList.toggle("completed");
 }
